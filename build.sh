e62c4c477e63efa175f186c5c62eefcfc4f47dbe
Philippe Marschall <philippe.marschall@gmail.com>
2025-08-30 14:51:57 +0200

    Introduce Crashes and Problems
    - add a new controller to produce crashes and problems
    - add more data
    - limit actuators
    - add run scripts
    - add build script
    - add workshop documentation

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -u
+set -e
+
+./mvnw package -DskipTests -Dcheckstyle.skip=true
+
+if [ ! -f docker/hsdis/hsdis-aarch64.so ]; then
+    curl --fail -o docker/hsdis/hsdis-aarch64.so --get "https://builds.shipilev.net/hsdis-custombuilt/hsdis-aarch64.so"
+fi
+
+if [ ! -f docker/hsdis/hsdis-amd64.so ]; then
+    curl --fail -o docker/hsdis/hsdis-amd64.so --get "https://builds.shipilev.net/hsdis-custombuilt/hsdis-amd64.so"
+fi
+
+#${JAVA_HOME}/bin/java \
+#  -Xmx512m \
+#  -XX:+UseSerialGC \
+#  -Djarmode=tools \
+#  -jar target/*.jar extract \
+#  --destination target/extracted
