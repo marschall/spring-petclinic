3c114a5c4d31a2cc697785c68ef0aaef176354ff
Philippe Marschall <philippe.marschall@gmail.com>
2025-08-30 17:55:59 +0200

    Crash harder

--- src/main/java/org/springframework/samples/petclinic/system/CrashController.java
+++ src/main/java/org/springframework/samples/petclinic/system/CrashController.java
@@ -15,6 +15,9 @@
  */
 package org.springframework.samples.petclinic.system;
 
+import static java.lang.foreign.MemorySegment.NULL;
+import static org.springframework.samples.petclinic.ffi.Mman.mmap;
+
 import java.lang.foreign.MemorySegment;
 import java.lang.foreign.ValueLayout;
 import java.lang.invoke.MethodHandles;
@@ -69,8 +72,8 @@ class CrashController {
 	@GetMapping("/crash3")
 	public String crash3() {
 		List<MemorySegment> segments = new ArrayList<>();
-		MemorySegment segment = requestMemory();
 		MemorySegment failed = MemorySegment.ofAddress(-1L);
+		MemorySegment segment = requestMemory();
 		while (!segment.equals(failed)) {
 			segments.add(segment);
 			segment = requestMemory();
@@ -91,11 +94,10 @@ class CrashController {
 
 	private static MemorySegment requestMemory() {
 		// request multiple pages
-		long len = Unistd.getpagesize() * 1024 * 1024; // 4 GB on Linux, 16 GB on macOS
+		long len = (long) Unistd.getpagesize() * 1024L; // 4 MB on Linux, 16 MB on macOS
 		int prot = Mman.PROT_READ() | Mman.PROT_WRITE();
 		int flags = Mman.MAP_ANON() | Mman.MAP_ANON();
-		MemorySegment segment = Mman.mmap(MemorySegment.NULL, len, prot, flags, -1, 0);
-		return segment;
+		return mmap(NULL, len, prot, flags, -1, 0L);
 	}
 
 	@ExceptionHandler(OutOfMemoryError.class)
