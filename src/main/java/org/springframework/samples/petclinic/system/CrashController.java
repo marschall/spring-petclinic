fad7fc3d534817520bcad24795304176819357cf
Philippe Marschall <philippe.marschall@gmail.com>
2025-08-30 14:51:57 +0200

    more crashes

--- src/main/java/org/springframework/samples/petclinic/system/CrashController.java
+++ src/main/java/org/springframework/samples/petclinic/system/CrashController.java
@@ -15,7 +15,18 @@
  */
 package org.springframework.samples.petclinic.system;
 
+import java.lang.invoke.MethodHandles;
+import java.nio.ByteBuffer;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.repository.config.RepositoryConfigurationDelegate;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /**
@@ -28,10 +39,41 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 class CrashController {
 
+	private static final Log LOG = LogFactory.getLog(MethodHandles.lookup().lookupClass());
+
 	@GetMapping("/oups")
 	public String triggerException() {
 		throw new RuntimeException(
 				"Expected: controller used to showcase what " + "happens when an exception is thrown");
+	}
+
+	@GetMapping("/crash1")
+	public String crash1() {
+		List<ByteBuffer> buffers = new ArrayList<>();
+		for (int i = 0; i < 1024; i++) {
+			buffers.add(ByteBuffer.allocate(Integer.MAX_VALUE));
+		}
+		return buffers.toString();
+	}
+
+	@GetMapping("/crash2")
+	public String crash2() {
+		List<ByteBuffer> buffers = new ArrayList<>();
+		for (int i = 0; i < 1024; i++) {
+			buffers.add(ByteBuffer.allocateDirect(Integer.MAX_VALUE));
+		}
+		return buffers.toString();
+	}
+
+	@GetMapping("/crash3")
+	public String crash3() {
+		return "crash3";
+	}
+
+	@ExceptionHandler(OutOfMemoryError.class)
+	public String outOfMemoryError(OutOfMemoryError e) {
+		LOG.warn("OutOfMemory", e);
+		return "error";
 	}
 
 }
