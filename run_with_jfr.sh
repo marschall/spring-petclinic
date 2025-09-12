9e3f9f14934a8eeb402dd4cd909d0846ed825a5b
Philippe Marschall <philippe.marschall@netcetera.com>
2025-09-12 10:47:24 +0200

    Record class loading times using JFR

---
+++ run_with_jfr.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+set -e
+set -u
+
+${JAVA_HOME}/bin/java \
+  -Xmx512m \
+  -XX:+UseSerialGC \
+  -XX:StartFlightRecording:filename=classloading-boot.jfr,dumponexit=true,settings=src/main/jfr/Classloading-only.jfc \
+  -jar target/*.jar
+
